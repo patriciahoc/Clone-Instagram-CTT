6e7249d0e85873c743c69c64999049f5defeee40
Patricia Hedra <patriciahedra@gmail.com>
2021-02-06 13:33:51 -0300

    codigo estavel

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { FiUser, FiHeart } from "react-icons/fi";
+import Logo from "./images";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <header>
+          <img src={Logo} alt="Logo Instagram" />
+          <div className="top-info"> 
+            <span>
+              <FiUser />
+              Seu nome
+            </span>
+          </div>
+        </header>
+      </div>
+      <div className="home">
+        <div className="post-container">
+          <div className="post">
+            <header>
+              <img src="https://randomuser.me/api/portraits/women/17.jpg" alt="user" />
+              <div className="post-user">
+                <strong>nasa</strong>
+                <span>Mars</span>
+              </div>
+            </header>
+            <div className="post-image">
+              <img src="https://www.publicdomainpictures.net/pictures/90000/velka/mars.jpg" alt="post" />
+            </div>
+            <div className="post-likes">
+              <FiHeart />
+            </div>
+            <p>Waving goodbye to a spacecraft.</p>
+          </div>
+          <div className="post">
+            <header>
+              <img src="https://randomuser.me/api/portraits/women/17.jpg" alt="user" />
+              <div className="post-user">
+                <strong>nasa</strong>
+                <span>Mars</span>
+              </div>
+            </header>
+            <div className="post-image">
+              <img src="https://www.publicdomainpictures.net/pictures/90000/velka/mars.jpg" alt="post" />
+            </div>
+            <div className="post-likes">
+              <FiHeart />
+            </div>
+            <p>Waving goodbye to a spacecraft.</p>
+          </div>
+        </div>
+        <div>
+        <div className="user-info">
+          <img
+            alt="Perfil"
+            src="https://randomuser.me/api/portraits/women/25.jpg"
+          />
+          <div className="user-bio">
+            <strong>Jeniffer</strong>
+            <span>jenicarvalho</span>
+          </div>
+        </div>
+          <div className="stories">
+            <h2>Stories</h2>
+
+            <div className="storie">
+              <div className="storie-image">
+                <img src="https://randomuser.me/api/portraits/men/11.jpg" alt="user" />
+              </div>
+              <div className="storie-user">
+                <strong>history</strong>
+                <span>há 2 horas</span>
+              </div>
+            </div>
+            <div className="storie">
+              <div className="storie-image">
+                <img src="https://randomuser.me/api/portraits/men/11.jpg" alt="user" />
+              </div>
+              <div className="storie-user">
+                <strong>history</strong>
+                <span>há 2 horas</span>
+              </div>
+            </div>
+          </div>
+        </div>        
+      </div>
+    </>
   );
 }
 
